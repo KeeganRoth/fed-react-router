2ca504ba2c311b8a9d1a5ef10bbb90f9c4b025ce
KeeganRoth <KeeganRoth@users.noreply.github.com>
2019-11-05 11:38:33 -0600

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./styles.css";
 import HomePage from "./pages/HomePage";
